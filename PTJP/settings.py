8098e786a606155a683ec1bf4166f55f64bac951
Talifhani Mulaudzi <mtalifhani54@gmail.com>
2022-09-13 02:37:29 +0200

    created an app for western cape trains

--- PTJP/settings.py
+++ PTJP/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'Lines',
     'train',
     'utils',
+    'western_cape',
 ]
 
 MIDDLEWARE = [
---
+++ western_cape/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WesternCapeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'western_cape'
