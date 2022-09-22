025065c7932f9086b75c4eba4fa287ff2a7d5707
Talifhani Mulaudzi <mtalifhani54@gmail.com>
2022-09-22 16:56:27 +0200

    trains stop

---
+++ western_cape/management/commands/updatetrainstops.py
@@ -0,0 +1,21 @@
+# updatetrainstops
+
+
+import pandas as pd
+import numpy as np
+from django.core.management.base import BaseCommand, CommandError
+from western_cape.models import Stop, Line, Arrival, Direction, Train, TrainStop
+
+class Command(BaseCommand):
+    help = 'Update Line'
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        trainstops = TrainStop.objects.all()
+        for arrival in trainstops:
+            print(arrival)
+            for stp in arrival.only_stops_at.all():
+                print(stp)
+            print()
