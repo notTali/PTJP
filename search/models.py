6c3b0c4d2bd6ef594e2c1c15034fa404bb23b618
Talifhani Mulaudzi <mtalifhani54@gmail.com>
2022-09-27 06:52:37 +0200

    models

--- search/models.py
+++ search/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 import uuid
 
+from western_cape.models import Arrival
+
 # Create your models here.
 class Search(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, null=False, blank=False)
@@ -8,3 +10,8 @@ class Search(models.Model):
     end_stop = models.CharField(max_length=200, null=False, blank=False)
     start_time = models.TimeField(null=True, blank=True)
     end_time = models.TimeField(null=True, blank=True)
+
+class Result(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, null=False, blank=False, unique=True)
+    title = models.CharField(default="",max_length=200, null=False, blank=False)
+    result = models.ManyToManyField(Arrival)
